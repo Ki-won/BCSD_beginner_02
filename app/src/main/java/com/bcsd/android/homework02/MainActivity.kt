138a885bc4238dc282504fa860088ff401f182c2
seo <seokw2240@gmail.com>
2022-09-29 15:35:46 +0900

    fix edittextview

--- app/src/main/java/com/bcsd/android/homework02/MainActivity.kt
+++ app/src/main/java/com/bcsd/android/homework02/MainActivity.kt
@@ -2,19 +2,22 @@ package com.bcsd.android.homework02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText = findViewById(R.id.editText) as EditText
-        val textView = findViewById(R.id.textView) as TextView
-        val button = findViewById(R.id.button) as Button
-        button.setOnClickListener(View.OnClickListener(){
-            @Override
-            fun onClick(view : View){
-                textView.setText(editText.text.toString())
-            }
+        val mainEditText = findViewById<EditText>(R.id.main_edit_text_view)
+        val mainTextView = findViewById<TextView>(R.id.main_show_text_view)
+        val mainButtonView = findViewById<Button>(R.id.main_btn_set_text)
+
+        mainButtonView.setOnClickListener(View.OnClickListener() {
+            mainTextView.text = mainEditText.text.toString()
         })
+    }
 }
